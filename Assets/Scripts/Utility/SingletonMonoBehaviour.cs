866ee2a7b48e0fda80786b8dd3717996e2819467
Es_Program <radwimps.september@gmail.com>
2015-07-09 01:30:49 +0900

    Singletonスクリプト追加

---
+++ Assets/Scripts/Utility/SingletonMonoBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Utility
+{
+  public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+  {
+    private static T instance;
+
+    public static T Instance
+    {
+      get
+      {
+        if(instance == null)
+        {
+          instance = FindObjectOfType<T>();
+
+#if UNITY_EDITOR
+          if(instance == null)
+            Debug.LogWarning(typeof(T) + "is nothing");
+#endif
+        }
+        return instance;
+      }
+    }
+
+    private void Awake()
+    {
+      if(!ReferenceEquals(this, Instance))
+      {
+        Destroy(this);
+        return;
+      }
+      DontDestroyOnLoad(this.gameObject);
+    }
+  }
+}
