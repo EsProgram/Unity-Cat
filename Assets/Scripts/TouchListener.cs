7a9fbcf824d5dc68759bb857c7015301e225a7cb
Es_Program <radwimps.september@gmail.com>
2015-07-09 21:13:00 +0900

    タッチ開始イベント処理の基本的な部分を追加

---
+++ Assets/Scripts/TouchListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TouchListener : SingletonMonoBehaviour<TouchListener>
+{
+  public void Awake()
+  {
+    TouchManager.Instance.TouchStart += OnTouchStart;
+  }
+
+  /// <summary>
+  /// タッチ開始時の動作
+  /// </summary>
+  void OnTouchStart(object sender, CustomInputEventArgs args)
+  {
+    Debug.Log("Call Touch Start");
+    //タッチした位置にタグ名がPlayerのオブジェクトがあればそれを取得する
+    var playerHit = Physics.RaycastAll(Camera.main.ScreenPointToRay(args.Input.ScreenPosition))
+      .FirstOrDefault(hit => { return hit.collider.gameObject.tag == "Player"; });
+
+    //プレイヤーのゲームオブジェクトを取得
+    var player = playerHit.collider != null ? playerHit.collider.gameObject : null;
+
+    //何かしらの処理
+  }
+
+
+}
